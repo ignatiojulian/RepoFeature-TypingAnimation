f13c5e2158e7d5855d52660fd9f963d3564a90c5
Ignatio <ignatio.tara@multipolar.com>
2022-01-07 13:35:10 +0700

    feature

--- SimpleTypingAnimation/SimpleTypingAnimation/ViewController.swift
+++ SimpleTypingAnimation/SimpleTypingAnimation/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textLabel: UILabel!
+    
+    var words = "Lets create An App"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(onClickView))
+        self.view.addGestureRecognizer(gesture)
     }
-
-
+    
+    @objc func onClickView() {
+        for item in words {
+            AudioServicesPlaySystemSound(1306)
+            textLabel.text! += "\(item)"
+            RunLoop.current.run(until: Date()+0.20)
+        }
+    }
 }
 
